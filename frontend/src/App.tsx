2f0ec8e3a64b7c7e186fa3a058add282956f0490
TobiasAagaard <Tobias909@live.dk>
2025-04-07 14:59:20 +0200

    Router domm made for frontend -- P1 Backend now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-     
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
